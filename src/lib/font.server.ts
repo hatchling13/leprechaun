0c42a5b45c29c281dc6e0ffc5fe59deb51247048
Jung Wook Park <hatchling3713@gmail.com>
2026-02-14 03:57:28 +0900

    feat: think once more about cloudflare specific logics

--- src/lib/font.server.ts
+++ src/lib/font.server.ts
@@ -1,29 +1,58 @@
 import { Config, Context, Effect, Layer } from "effect";
 import { FileSystem, Path } from "@effect/platform";
+import { env } from "cloudflare:workers";
+import { NodeContext } from "@effect/platform-node";
 
 export class FontService extends Context.Tag("FontService")<
   FontService,
   { readonly getFonts: Effect.Effect<{ name: string; data: ArrayBuffer }[], Error> }
 >() {}
 
-const fetchFonts = Effect.gen(function* () {
-  const fs = yield* FileSystem.FileSystem;
-  const pathService = yield* Path.Path;
+const getArrayBufferInDevelopment = (path: string) =>
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem;
+    const uint8 = yield* fs.readFile(path);
+    const buffer = uint8.buffer;
+
+    if (buffer instanceof ArrayBuffer) {
+      return buffer;
+    }
+
+    const copy = new ArrayBuffer(buffer.byteLength);
+    new Uint8Array(copy).set(new Uint8Array(buffer));
+    return copy;
+  });
+
+const fetchFontsDevelopment = Effect.gen(function* () {
   const paths = yield* Config.array(Config.string(), "FONT_PATHS");
+  const pathService = yield* Path.Path;
   const concurrency = yield* Config.integer("FONT_LOAD_CONCURRENCY").pipe(Config.withDefault(10));
 
   return yield* Effect.all(
     paths.map((path) =>
-      fs.readFile(path).pipe(
-        Effect.map((uint8) => {
-          const destination = new Uint8Array(new ArrayBuffer(uint8.byteLength));
-          destination.set(uint8);
+      getArrayBufferInDevelopment(path).pipe(
+        Effect.map((data) => ({ name: pathService.basename(path), data })),
+      ),
+    ),
+    { concurrency },
+  );
+});
 
-          return {
-            name: pathService.basename(path),
-            data: destination.buffer,
-          };
-        }),
+const getArrayBufferInProduction = (path: string) =>
+  Effect.gen(function* () {
+    const response = yield* Effect.tryPromise(() => env.ASSETS.fetch(new URL(path, "http://i")));
+    return yield* Effect.tryPromise(() => response.arrayBuffer());
+  });
+
+const fetchFontsProduction = Effect.gen(function* () {
+  const paths = yield* Config.array(Config.string(), "FONT_PATHS");
+  const pathService = yield* Path.Path;
+  const concurrency = yield* Config.integer("FONT_LOAD_CONCURRENCY").pipe(Config.withDefault(10));
+
+  return yield* Effect.all(
+    paths.map((path) =>
+      getArrayBufferInProduction(path).pipe(
+        Effect.map((data) => ({ name: pathService.basename(path), data })),
       ),
     ),
     { concurrency },
@@ -33,16 +62,18 @@ const fetchFonts = Effect.gen(function* () {
 export const FontServiceLive = Layer.effect(
   FontService,
   Effect.gen(function* () {
-    const fs = yield* FileSystem.FileSystem;
-    const path = yield* Path.Path;
-
-    const getFonts = yield* fetchFonts.pipe(
-      Effect.provideService(FileSystem.FileSystem, fs),
-      Effect.provideService(Path.Path, path),
-      Effect.mapError((err) => new Error(String(err))),
-      Effect.cached,
-    );
+    const getFonts = import.meta.env.DEV
+      ? yield* fetchFontsDevelopment.pipe(
+          Effect.provide(NodeContext.layer),
+          Effect.mapError((err) => new Error(String(err))),
+          Effect.cached,
+        )
+      : yield* fetchFontsProduction.pipe(
+          Effect.provide(Path.layer),
+          Effect.mapError((err) => new Error(String(err))),
+          Effect.cached,
+        );
 
     return FontService.of({ getFonts });
   }),
-);
+).pipe(Layer.provide(import.meta.env.DEV ? NodeContext.layer : Path.layer));
